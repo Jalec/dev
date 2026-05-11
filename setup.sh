df0c955f8031c2ad22fb36491d2696b12cf57f55
Jalec <jordi.alejandre10@gmail.com>
2026-05-11 17:41:17 +0200

    feat: update setup script for flag configuration

--- setup.sh
+++ setup.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-# Copy contents of nvim dev configuration
+scope=all
 
-cp -R ~/dev/nvim-dev/. ~/.config/nvim
+while getopts ank flag
+do
+  case "${flag}" in
+    a) echo "You selected complete setup copy"
+       scope=all;;
+    n) echo "You selected nvim setup copy"
+       scope=nvim;;
+    k) echo "You selected kitty setup copy"
+       scope=kitty;;
+  esac
+done
+
+if [[ "$scope" == "all" || "$scope" == "nvim" ]]; then
+  # Copy contents of nvim dev configuration
+  mkdir -p ~/.config/nvim
+  cp -R ~/dev/nvim-dev/. ~/.config/nvim
+fi
+
+if [[ "$scope" == "all" || "$scope" == "kitty" ]]; then
+  # Copy contents of kitty dev configuration
+  mkdir -p ~/.config/kitty
+  cp -R ~/dev/kitty-dev/. ~/.config/kitty
+fi
+
+
+
