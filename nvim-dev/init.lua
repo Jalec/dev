d51befc009bfd25bc286da71db07097a8a55f762
Jalec <jordi.alejandre10@gmail.com>
2026-08-14 01:38:19 +0200

    add lazy.vim + plugins

--- nvim-dev/init.lua
+++ nvim-dev/init.lua
@@ -15,3 +15,49 @@ vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>")
 vim.keymap.set("i", "jj", "<Esc>")
 
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>")
+
+
+-- specific configs
+vim.api.nvim_create_autocmd("FileType",
+  {
+    pattern = "python",
+    callback = function()
+      vim.opt_local.tabstop = 4
+      vim.opt_local.shiftwidth = 4
+      vim.opt_local.softtabstop = 4
+      vim.opt_local.expandtab = true
+    end,
+  })
+
+vim.api.nvim_create_autocmd("FileType",
+  {
+    pattern = "python",
+    callback = function()
+      vim.treesitter.start()
+    end,
+  })
+
+-- lazy.nvim config
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  }, 
+})
